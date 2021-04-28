5123bf50ccced7dfdc7f8ce494ee83287249102b
陈王翾 <524884173@qq.com>
2021-04-28 23:18:24 +0800

    柔性数组的应用，涉及到realloc和malloc的动态开辟空间

---
+++ struct1.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct S
+{
+	int n;
+	int arr[];
+};
+
+int main()
+{
+	struct S* ps = (struct S*)malloc(sizeof(struct S)+5*sizeof(int));
+	ps->n = 100;
+	int i = 0;
+	for (i = 0;i < 5;i++)
+	{
+		ps->arr[i] = i;
+	}
+	struct S* pre =(struct S*) realloc(ps,44);
+	if (pre != NULL)
+	{
+		ps = pre;//0,1,2,3,4
+	}
+	for (i = 5;i < 10;i++)
+	{
+		ps->arr[i] = i;
+	}
+	for (i = 0;i < 10;i++)
+	{
+		printf("%d ",ps->arr[i]);
+	}
+	// Õ∑≈
+	free(ps);
+	ps = NULL;
+	return 0;
+}
