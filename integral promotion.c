86bc0ff659b0129655f1c001437bb5b1e60f29ff
陈王翾 <524884173@qq.com>
2021-05-03 22:51:32 +0800

    关于整型提升的两个简单例子

---
+++ integral promotion.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	char a = 0xb6;
+	short b = 0xb600;
+	int c = 0xb6000000;
+	if (a == 0xb6)//运算中整型提升，a的值为-0x4a
+		printf("a");
+	if (b == 0xb600)//-0x4a00
+		printf("b");
+	if (c == 0xb6000000)
+		printf("c");
+	return 0;
+}
